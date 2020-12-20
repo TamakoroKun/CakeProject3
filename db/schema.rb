7f13124b7d17bd5bad0e2b6c102cc70cc1e83ab2
i29ri <g1g1yuto@gmail.com>
2020-12-20 05:08:48 +0000

    adminmodel-add

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_12_19_085710) do
-
+ActiveRecord::Schema.define(version: 2020_12_20_043232) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -23,6 +21,18 @@ ActiveRecord::Schema.define(version: 2020_12_19_085710) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
   create_table "cards", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "customer_id", null: false
@@ -30,7 +40,6 @@ ActiveRecord::Schema.define(version: 2020_12_19_085710) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
-
   end
 
   create_table "customers", force: :cascade do |t|
@@ -53,7 +62,6 @@ ActiveRecord::Schema.define(version: 2020_12_19_085710) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
-
   create_table "items", force: :cascade do |t|
     t.integer "genre_id"
     t.string "image_id"
@@ -61,6 +69,9 @@ ActiveRecord::Schema.define(version: 2020_12_19_085710) do
     t.text "introduction"
     t.integer "price"
     t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string "name"
