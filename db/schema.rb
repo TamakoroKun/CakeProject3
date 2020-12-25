ba80fcb2dea7d3361c5abdd93234be5f424c6899
EC2 Default User <ec2-user@ip-172-31-32-167.ec2.internal>
2020-12-25 08:43:59 +0000

    カートエラー修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_25_061912) do
+ActiveRecord::Schema.define(version: 2020_12_24_050601) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 2020_12_25_061912) do
     t.integer "shipping_cost"
     t.integer "total_payment"
     t.integer "status", default: 0, null: false
+    t.integer "customer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
