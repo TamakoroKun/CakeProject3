5c8744f268cc7c905359e175d77cf7b5dc28c6ca
i29ri <g1g1yuto@gmail.com>
2020-12-26 07:16:25 +0000

    confirm-fix

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,6 +1,6 @@
 class Public::OrdersController < ApplicationController
-    
-    
+
+
   def index
     @customer = current_customer
     @orders = current_customer.orders
@@ -8,13 +8,13 @@ class Public::OrdersController < ApplicationController
     #@orderedItemHoge = @customer.orderedItem
     #Itemsを呼ぶ必要があるが、Itemにはaddressなどが入ってない。これをeachでまとめて呼ぶ方法をメンターに相談する。
   end
-  
+
   def new
     # @customer = current_customer
     # @addresses = current_customer.addresses
     @order = Order.new
-  end  
-  
+  end
+
   def confirm
     @order = Order.new
     # パラメータで送ったものは全部受け取る
@@ -23,7 +23,7 @@ class Public::OrdersController < ApplicationController
     # ラジオボタンで分けるが、それをどう認識するか。(paramsをif)
     # 直に打って送るタイプのものは場合分けが必要ない(payment_methodのこと)
     # 送料、合計金額もまた@orderに入れたい。
-    
+
     #例）@order.shipping_cost = 800
     #if params[:order][:address_option] == "0"
     # @order.address = current_customer.addres
@@ -31,43 +31,43 @@ class Public::OrdersController < ApplicationController
       @order.address = current_customer.address
       #binding.pry
 
-    
+
     elsif params[:order][:address_option] == "1"
       @order.address = current_customer.addresses
       #current_customer.addressesは仮。adressesをプルダウンから選んでadressに入れたい。
-      
+
     elsif params[:order][:address_option] == "2"
     　@address = Address.new(address_params)
     　@address.customer_id = current_customer.id
-    　
+
     else
-      
+
     end
     @customer = current_customer
   end
-  
+
   def create
     #binding.pry
     @order = current_customer.orders.new()
-  end  
-  
+  end
+
   def done
-    
-  end  
-  
+
+  end
+
   def show
         @customer = Customer.find(params[:id])
         #@orderedItemHoge = @customer.orderedItem
-  end  
-    
-    
+  end
+
+
   private
-  
-  
+
+
   def address_params
     params.require(:address).permit(:postal_code, :name, :address)
   end
-  
+
 end
 
 
